771609c7a841a8e9d9ea081ada7c94213075220e
kmwtr <0x550x45@gmail.com>
2021-02-07 21:47:37 +0900

    add def gif

--- py/make_thumbnail.py
+++ py/make_thumbnail.py
@@ -9,8 +9,8 @@ import debug_config
 from load_settings import load_settings
 from image_list import image_list
 
-def make_thumbnail(image_lists: dict, dirs_list: dict):
-    log.debug('-> make_thumbnail()')
+def make_image_thumbnail(image_lists: dict, dirs_list: dict):
+    log.debug('-> make_image_thumbnail()')
 
     # jpg, png をサムネイル処理する
     candidate_img_list = image_lists['candidate_img_list']
@@ -41,34 +41,47 @@ def make_thumbnail(image_lists: dict, dirs_list: dict):
         log.debug('saved_tmp-tmb: ' + 'tmb_' + tmp_name + '.png')
         
         # mozjpg を呼び出してjpgに圧縮する
-        log.debug('-> run_mozjpg()')
+        log.debug('| -> run MozJPEG')
         
         output_path = dirs_list['tmb_img_dir'] + 'tmb_' + tmp_name + '.jpg'
 
         # 圧縮、中間ファイルとして出力
         cp = subprocess.run(['cjpeg-static', '-quality', '90', '-outfile', dirs_list['tmb_img_dir'] + 'intermediate_img.jpg', tmp_path], encoding='utf-8', stdout=subprocess.PIPE)
-        print(cp)
+        log.debug(cp)
 
         # Exif 等メタデータを削除、最終ファイル出力
         cp = subprocess.run(['jpegtran-static', '-copy', 'none', '-optimize', '-outfile', output_path, dirs_list['tmb_img_dir'] + 'intermediate_img.jpg'], encoding='utf-8', stdout=subprocess.PIPE)
-        print(cp)
+        log.debug(cp)
 
         log.debug('saved_tmb: ' + 'tmb_' + tmp_name + '.jpg')
 
         # 中間ファイル削除
+        os.remove(tmp_path)
         os.remove(dirs_list['tmb_img_dir'] + 'intermediate_img.jpg')
-    
-    
+
+
+def make_gif_thumbnail(image_lists: dict, dirs_list: dict):
+    log.debug('-> make_gif_thumbnail()')
+
     # gif をサムネイル処理する
     candidate_gif_list = image_lists['candidate_gif_list']
 
     for i in range(len(candidate_gif_list)):
-        print('Hello')
+        # Gifsicle を呼び出して圧縮する（PIL でやるのは諦めた。）
+        log.debug('| -> run Gifsicle')
+        
+        output_path = dirs_list['tmb_img_dir'] + 'tmb_' + candidate_gif_list[i]
+        log.debug(dirs_list['src_img_dir'] + candidate_gif_list[i])
 
+        # 圧縮、中間ファイルとして出力
+        cp = subprocess.run(['gifsicle','--resize', '240x180', '--optimize=3', '--colors', '256', '--lossy=40', dirs_list['src_img_dir'] + candidate_gif_list[i], '>', output_path], shell=True, encoding='utf-8', stdout=subprocess.PIPE) # shell=True 重要!
+        log.debug(cp)
 
 # -------------------------------------------------
 
 if __name__ == '__main__':
     dirs_list = load_settings()
-    make_thumbnail(image_list(dirs_list), dirs_list)
+    image_lists = image_list(dirs_list)
+    make_image_thumbnail(image_lists, dirs_list)
+    make_gif_thumbnail(image_lists, dirs_list)
     #os.system('PAUSE')
