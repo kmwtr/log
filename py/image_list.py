1653b3bc88470d4baa8d699e46ff382883e49e7f
kmwtr <0x550x45@gmail.com>
2022-05-19 10:22:32 +0900

    refactoring

--- py/image_list.py
+++ py/image_list.py
@@ -7,6 +7,44 @@ from PIL import Image
 import debug_config
 from load_settings import load_settings
 
+
+def file_list_maker(file_extension: str, source_image_list: list) -> list:
+    log.debug('-> file_list_maker()')
+
+    match_extension_regex = re.compile(file_extension, re.I)
+    return match_extension_regex.findall(str(source_image_list))
+
+def large_file_searcher(file_list: list, threshold_byte: int, dir_settings: dict, resolution_threshold: int) -> list:
+    log.debug('-> large_file_searcher()')
+
+    large_file_list = []   # 大きい jpg, png のリスト
+
+    for i in range(len(file_list)):
+        if os.path.getsize(dir_settings['src_img_dir'] + file_list[i]) > threshold_byte:
+            large_file_list.append(file_list[i])
+        elif resolution_threshold != 0:
+            # resolution_threshold が有効数字であれば解像度も確認する
+            tmp_obj = Image.open(dir_settings['src_img_dir'] + file_list[i])
+            tmp_reso = tmp_obj.width * tmp_obj.height
+            # ファイルサイズが小さくても解像度が大きければリストに入れる
+            if tmp_reso >= resolution_threshold:
+                large_file_list.append(file_list[i])
+
+    return large_file_list
+
+def processing_candidate_maker(large_file_list: list, thumbnail_image_list: list) -> list:
+    log.debug('-> processing_candidate_maker()')
+
+    candidate_list = []
+    for i in range(len(large_file_list)):
+        tmp_name = large_file_list[i].split('.')[0]
+        if not any(s.startswith(r'tmb_' + tmp_name) for s in thumbnail_image_list):
+            candidate_list.append(large_file_list[i])
+    
+    return candidate_list
+
+# -------------------------------------------------
+
 def image_list(dir_settings: dict) -> dict:
     log.debug('-> image_list()')
 
@@ -14,8 +52,8 @@ def image_list(dir_settings: dict) -> dict:
     source_image_list       = os.listdir(dir_settings['src_img_dir'])
     thumbnail_image_list    = os.listdir(dir_settings['tmb_img_dir'])
 
-    log.debug('source_image_list:    ' + str(source_image_list))
-    log.debug('thumbnail_image_list: ' + str(thumbnail_image_list))
+    log.debug('source_image_list:    \n' + str(source_image_list))
+    log.debug('thumbnail_image_list: \n' + str(thumbnail_image_list))
 
     # とりあえず
     for i in range(len(source_image_list)):
@@ -23,57 +61,43 @@ def image_list(dir_settings: dict) -> dict:
             log.error('画像ファイル名にスペースが含まれています。ルール通り命名されたファイルしか処理できません。: ' + source_image_list[i])
             return None
 
-    # jpg, png のリストを作成
-    match_extension_regex = re.compile(r'\w+\.jpg|\w+\.png', re.I)
-    jpg_png_list = match_extension_regex.findall(str(source_image_list))
+    # jpg, png, gif, mp4 のリストを作成
+    jpg_png_list    = file_list_maker(r'\w+\.jpg|\w+\.png', source_image_list)
+    gif_list        = file_list_maker(r'\w+\.gif', source_image_list)
+    mp4_list        = file_list_maker(r'\w+\.mp4', source_image_list)
+
+    # 大きい jpg, png, gif, mp4 のリストを作成
+    large_img_list    = large_file_searcher(jpg_png_list, 128000, dir_settings, 0)
+    large_gif_list      = large_file_searcher(gif_list, 512000, dir_settings, 43200) # 240 * 180 を超える場合
+    large_mp4_list      = large_file_searcher(mp4_list, 512000, dir_settings, 0)
+
+    log.debug('large_img_list: \n' + str(large_img_list))
+    log.debug('large_gif_list: \n' + str(large_gif_list))
+    log.debug('large_mp4_list: \n' + str(large_mp4_list))
+
+    # サムネイルを持たない大きい jpg, png, gif, mp4 リストを作成
+    candidate_img_list = processing_candidate_maker(large_img_list, thumbnail_image_list)
+    candidate_gif_list = processing_candidate_maker(large_gif_list, thumbnail_image_list)
+    candidate_mp4_list = processing_candidate_maker(large_mp4_list, thumbnail_image_list)
+
+    log.debug('candidate_img_list: \n' + str(candidate_img_list))
+    log.debug('candidate_gif_list: \n' + str(candidate_gif_list))
+    log.debug('candidate_mp4_list: \n' + str(candidate_mp4_list))
+
+    image_list_dict = {
+        'source_image_list': source_image_list, 
+        'thumbnail_image_list': thumbnail_image_list, 
+        'large_img_list': large_img_list, 
+        'large_gif_list': large_gif_list, 
+        'large_mp4_list': large_mp4_list, 
+        'candidate_img_list': candidate_img_list, 
+        'candidate_gif_list': candidate_gif_list,
+        'candidate_mp4_list': candidate_mp4_list,
+        }
     
-    # gif のリストを作成
-    match_extension_regex = re.compile(r'\w+\.gif', re.I)
-    gif_list = match_extension_regex.findall(str(source_image_list))
+    log.debug('image_list_dict: \n' + str(image_list_dict))
 
-    # 大きい jpg, png, gif のリストを作成
-    img_threshold_byte = 128000 # 閾値 for jpg, png
-    gif_threshold_byte = 512000 # 閾値 for gif
-    large_image_list = []   # 大きい jpg, png のリスト
-    large_gif_list = []     # 大きい gif のリスト
-
-    for i in range(len(jpg_png_list)):
-        if os.path.getsize(dir_settings['src_img_dir'] + jpg_png_list[i]) > img_threshold_byte:
-            large_image_list.append(jpg_png_list[i])
-
-    for i in range(len(gif_list)):
-        if os.path.getsize(dir_settings['src_img_dir'] + gif_list[i]) > gif_threshold_byte:
-            # gif は解像度も確認する
-            tmp_obj = Image.open(dir_settings['src_img_dir'] + gif_list[i])
-            tmp_reso = tmp_obj.width * tmp_obj.height
-            # 240 * 180 を超える場合
-            if tmp_reso > 43200:
-                large_gif_list.append(gif_list[i])
-
-    log.debug('large_image_list:     ' + str(large_image_list))
-    log.debug('large_gif_list:       ' + str(large_gif_list))
-
-    # サムネイルを持たない大きい jpg, png リスト
-    candidate_img_list = []
-    for i in range(len(large_image_list)):
-        tmp_name = large_image_list[i].split('.')[0]
-        if not any(s.startswith(r'tmb_' + tmp_name) for s in thumbnail_image_list):
-            candidate_img_list.append(large_image_list[i])
-
-    log.debug('candidate_img_list:   ' + str(candidate_img_list))
-
-    # サムネイルを持たない大きい gif リスト
-    candidate_gif_list = []
-    for i in range(len(large_gif_list)):
-        tmp_name = large_gif_list[i].split('.')[0]
-        if not any(s.startswith(r'tmb_' + tmp_name) for s in thumbnail_image_list):
-            candidate_gif_list.append(large_gif_list[i])
-
-    log.debug('candidate_gif_list:   ' + str(candidate_gif_list))
-
-    image_list_dict = {'source_image_list': source_image_list, 'thumbnail_image_list': thumbnail_image_list, 'large_image_list':large_image_list, 'large_gif_list': large_gif_list, 'candidate_img_list': candidate_img_list, 'candidate_gif_list': candidate_gif_list}
     return image_list_dict
-
 
 # -------------------------------------------------
 
