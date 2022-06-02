f7ecf873b2dc7bdc98b0c2bd9d6c98eedf0a2161
kmwtr <0x550x45@gmail.com>
2022-06-02 10:50:35 +0900

    gui test

--- py/tst_tmp_gui.py
+++ py/tst_tmp_gui.py
@@ -1,6 +1,5 @@
-import sys
 import tkinter
-from PIL import Image, ImageTk
+import customtkinter
 
 def tmp_gui():
     root = tkinter.Tk()
@@ -14,9 +13,29 @@ def tmp_gui():
     
     root.mainloop()
 
+def test():
+    
+    customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+    #customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+    customtkinter.set_default_color_theme("./custom_theme.json")
+
+    app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+    app.geometry("480x360")
+    app.title("CustomTkinter simple_example.py")
+
+    def button_function():
+        print("button pressed")
+
+    # Use CTkButton instead of tkinter Button
+    button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+    button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+    app.mainloop()
+
 # -------------------------------------------------
 
 if __name__ == '__main__':
-    tmp_gui()
+    #tmp_gui()
+    test()
     print('俺たちの旅はまだ始まったばかり…')
     #os.system('PAUSE')
